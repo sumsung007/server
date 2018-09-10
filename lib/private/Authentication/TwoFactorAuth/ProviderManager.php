0ae9bcc13b68a88fbdebc55bb40234edb8c4baa1
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-09-10 17:02:37 +0200

    Only allow 2FA state changs if providers support the operation
    Ref https://github.com/nextcloud/server/issues/11019.
    
    This ensures that when an admin tries to enable or disable a provider,
    the provider actually allows the state change without user interaction.
    
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ lib/private/Authentication/TwoFactorAuth/ProviderManager.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2018 Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @author 2018 Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OC\Authentication\TwoFactorAuth;
+
+use OC\Authentication\Exceptions\InvalidProviderException;
+use OCP\Authentication\TwoFactorAuth\IActivatableByAdmin;
+use OCP\Authentication\TwoFactorAuth\IDeactivableByAdmin;
+use OCP\Authentication\TwoFactorAuth\IDeactivatableByAdmin;
+use OCP\IUser;
+
+class ProviderManager {
+
+	/** @var Manager */
+	private $twoFactorManager;
+
+	public function __construct(Manager $twoFactorManager) {
+		$this->twoFactorManager = $twoFactorManager;
+	}
+
+	/**
+	 * Try to enable the provider with the given id for the given user
+	 *
+	 * @param IUser $user
+	 *
+	 * @return bool whether the provider supports this operation
+	 */
+	public function tryEnableProviderFor(string $providerId, IUser $user): bool {
+		$provider = $this->twoFactorManager->getProvider($user, $providerId);
+		if (is_null($provider)) {
+			throw new InvalidProviderException($providerId);
+		}
+
+		if ($provider instanceof IActivatableByAdmin) {
+			$provider->enableFor($user);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Try to disable the provider with the given id for the given user
+	 *
+	 * @param IUser $user
+	 *
+	 * @return bool whether the provider supports this operation
+	 */
+	public function tryDisableProviderFor(string $providerId, IUser $user): bool {
+		$provider = $this->twoFactorManager->getProvider($user, $providerId);
+		if (is_null($provider)) {
+			throw new InvalidProviderException($providerId);
+		}
+
+		if ($provider instanceof IDeactivatableByAdmin) {
+			$provider->disableFor($user);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
